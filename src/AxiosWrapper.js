a04bf3c7be7f76940ae8ee50f557ed5b77dcb9ff
Daniel Thompson <danl_thompson@hotmail.com>
2022-09-12 13:25:23 -0400

    adding trace messages

--- src/AxiosWrapper.js
+++ src/AxiosWrapper.js
@@ -3,6 +3,7 @@ const axios = require('axios').default;
 var host = "http://localhost:8088"; // could parameterize this TTD
 
 async function vMixSend(endpoint,params) {
+      if (process.env["VMIX_TRACE"]) console.log(params)
       const response = axios.get(host+endpoint, {params})
       return response;
 } 
